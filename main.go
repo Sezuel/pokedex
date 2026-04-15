3dac138afd805d0e42d31eb66e60e1160bcd6d81
Sezuel <jdragon660@yahoo.com>
2026-04-15 08:43:15 -0700

    Add Caching

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"time"
-
 	"github.com/sezuel/pokedex/internal/pokeapi"
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
 		pokeapiClient: pokeClient,
 	}
